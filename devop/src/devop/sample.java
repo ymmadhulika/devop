5b6d7306002e57dd6a7c671408b638765e7fb290
ymmadhulika <ymmadhulika@gmail.com>
2023-09-29 14:40:45 +0530

    i committed the files.

---
+++ devop/src/devop/sample.java
@@ -0,0 +1,12 @@
+package devop;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi i am analysing the code");
+		System.out.println("hi i am analysing the code");
+		System.out.println("hi i am analysing the code");
+	}
+
+}
